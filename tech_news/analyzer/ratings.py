6e3d38129ff4267c2097109bebde6e36732009a6
Maria <marycarol.00@gmail.com>
2024-09-12 17:50:25 -0300

    feat: listar top cinco categorias

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,13 @@
-# Requisito 10
+from tech_news.database import db
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    top_5 = [
+        {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1, "_id": 1}},
+        {"$limit": 5},
+    ]
+
+    categories = list(db.news.aggregate(top_5))
+    result = [category["_id"] for category in categories]
+    return result
